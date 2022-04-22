35e24362e788ff9f591e28a54453e938f963f70c
Ameya Rele <rele.ameya@gmail.com>
2022-04-22 10:54:26 +0530

    Added changes to README and trigger binding sample from triggerbindings branch

---
+++ samples/TriggerBindingSamples/ProductsTrigger.cs
@@ -0,0 +1,26 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Azure.WebJobs.Extensions.Sql.Samples.Common;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Sql.Samples.TriggerBindingSamples
+{
+    public static class ProductsTrigger
+    {
+        [FunctionName("ProductsTrigger")]
+        public static void Run(
+            [SqlTrigger("[dbo].[Products]", ConnectionStringSetting = "SqlConnectionString")]
+            IEnumerable<SqlChangeTrackingEntry<Product>> changes,
+            ILogger logger)
+        {
+            foreach (var change in changes)
+            {
+                Product product = change.Data;
+                logger.LogInformation($"Change occurred to Products table row: {change.ChangeType}");
+                logger.LogInformation($"ProductID: {product.ProductID}, Name: {product.Name}, Cost: {product.Cost}");
+            }
+        }
+    }
+}
